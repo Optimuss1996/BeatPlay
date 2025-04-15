85f376715cb1a2623821ae12f5050ccdd0dc1e20
Optimus <alisalahi19951995@gmail.com>
2025-04-15 18:03:30 +0330

    adding final  playlist trend in homepage

--- app/liked/components/Player.tsx
+++ app/liked/components/Player.tsx
@@ -1,20 +1,20 @@
-"use client";
+// "use client";
 
-import { useGetPlaylistSongById } from "@/hooks/useGetSongById";
-import usePlayer from "@/hooks/usePlayer";
-import PlayerContent from "@/app/components/PlayerContent";
+// import { useGetPlaylistSongById } from "@/hooks/useGetSongById";
+// import usePlayer from "@/hooks/usePlayer";
+// import PlayerContent from "@/app/components/PlayerContent";
 
-export default function Player() {
-  const { activeId } = usePlayer();
-  const { song } = useGetPlaylistSongById(activeId);
-  console.log("🚨track by id :", song);
-  if (!song || !activeId) {
-    return null;
-  }
+// export default function Player() {
+//   const { activeId } = usePlayer();
+//   const { song } = useGetPlaylistSongById(activeId);
+//   console.log("🚨track by id :", song);
+//   if (!song || !activeId) {
+//     return null;
+//   }
 
-  return (
-    <div className=" w-full h-[80px] px-6 py-3 fixed bottom-0 border-t  border-gray-400 border-opacity-75 bg-purple-200 shadow-lg dark:bg-slate-900 z-50">
-      <PlayerContent key={song.song_id} song={song} />
-    </div>
-  );
-}
+//   return (
+//     <div className=" w-full h-[80px] px-6 py-3 fixed bottom-0 border-t  border-gray-400 border-opacity-75 bg-purple-200 shadow-lg dark:bg-slate-900 z-50">
+//       <PlayerContent key={song.song_id} song={song} />
+//     </div>
+//   );
+// }
